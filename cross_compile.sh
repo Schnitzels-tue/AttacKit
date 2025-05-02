6bec0265493e3fb48c23b6dcaef4d2e1d451248c
Jason Fu <r.fu1@student.tue.nl>
2025-05-02 14:40:45 +0200

    fix for arm docker

--- cross_compile.sh
+++ cross_compile.sh
@@ -2,8 +2,8 @@
 
 echo "This is a temporary solution that bears inconsistent results, in the future switch to on machine building or some other solution."
 
-docker build --progress=plain -f Dockerfile.amd64-linux-static -t amd64-linux-static-builder --output type=local,dest=./cross_build . && echo "amd64-linux-static finished" &
+docker build --progress=quiet -f Dockerfile.amd64-linux-static -t amd64-linux-static-builder --output type=local,dest=./cross_build . &
 
-# docker build --progress=plain -f Dockerfile.amd64-linux -t amd64-linux-builder --output type=local,dest=./cross_build . && echo "amd64-linux finished" &
-# docker build -f Dockerfile.arm64-linux -t arm64-linux-builder --output type=local,dest=./cross_build . && echo "arm64-linux finished" &
+docker build --progress=quiet -f Dockerfile.amd64-linux -t amd64-linux-builder --output type=local,dest=./cross_build . &
+docker build --progress=quiet -f Dockerfile.arm64-linux -t arm64-linux-builder --output type=local,dest=./cross_build . $
 wait
