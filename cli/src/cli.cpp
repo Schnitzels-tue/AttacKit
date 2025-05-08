05236ca3770d23860101a349d46aee5ceb281bd9
Quinnca77 <50358541+Quinnca77@users.noreply.github.com>
2025-05-08 22:54:42 +0200

    Delete commented code

--- cli/src/cli.cpp
+++ cli/src/cli.cpp
@@ -10,9 +10,6 @@ int main(int argc, char* argv[]) {
     std::vector<std::string> args(argv + 1, argv + argc);
     CLIParser parser(args);
     parser.add_flag("help", [&parser](const std::vector<std::string>&) { parser.help(); }, "Opens this help menu", 0);
-    // parser.add_flag("arp", []() {ARP::poisonArp();}, "env    Performs an ARP poisoning attack on the device with name 'env'");
-    // parser.add_flag("dns", []() { DNS::spoofDns(); }, "dummy    Lorem Ipsum");
-    // parser.add_flag("ssl", []() {SSL::stripSSL();}, "dummy    Lorem Ipsum");
 
     std::optional<std::vector<CLIParser::InvokeableFunction>> parsedFunctions = parser.parse();
     if (parsedFunctions) {
