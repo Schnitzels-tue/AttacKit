0660c427cec200707228389c349681d839d65ead
Quinn <quinn.caris@gmail.com>
2025-05-09 08:58:37 +0200

    CLI implementation (#12)
    * cli skeleton
    
    * Rework
    
    * Refactor
    
    * Refactor to vector instead of unordered map
    
    * Refactor flag structure again
    
    * CLI implementation done (might have bugs)
    
    * Updated with example usage
    
    * Delete outdated comment
    
    * Fix bug
    
    * Refactor branches to be more logical
    
    * Delete helper
    
    * Delete commented code
    
    * Insert newlines

---
+++ cli/internal/helper/CLIParser.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <functional>
+#include <optional>
+#include <string>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+
+class CLIParser {
+    using AnyFunction = std::function<void(const std::vector<std::string>&)>;
+
+    struct Flag {
+        std::string flagName;
+        char flagChar;
+        AnyFunction flagFunction;
+        std::string flagHelpText;
+        int amountOfArguments;
+    };
+
+
+    std::string helpText;
+    std::vector<std::string> args;
+
+    std::vector<Flag> allFlags;
+    std::vector<Flag> setFlags;
+
+public:
+    struct InvokeableFunction {
+        AnyFunction function;
+        std::vector<std::string> arguments;
+    };
+
+    template <typename... Args>
+    explicit CLIParser(Args... args) : args({ args... }), 
+        helpText("This application is meant to provide the necessary functionality to perform ARP poisoning, "
+        "DNS spoofing and SSL stripping.\n"
+        "Usage: AttacKit [options] \n"
+        "  options: \n") {}
+
+    void print();
+    std::optional<std::vector<InvokeableFunction>> parse();
+    static void invokeFunction(const AnyFunction&, const std::vector<std::string>&);
+
+    int findCharFlag(char);
+    int findFlagName(const std::string&);
+    
+    void help();
+
+    std::string generate_flags_text();
+    void add_flag(const std::string& flagName, const AnyFunction& associatedFunction, const std::string& helpText, int amountOfArguments);
+};
--- cli/src/cli.cpp
+++ cli/src/cli.cpp
@@ -1,1 +1,21 @@
-int main() { return 0; }
+#include "helper/CLIParser.h"
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main(int argc, char* argv[]) {
+
+    // Parse command line arguments
+    std::vector<std::string> args(argv + 1, argv + argc);
+    CLIParser parser(args);
+    parser.add_flag("help", [&parser](const std::vector<std::string>&) { parser.help(); }, "Opens this help menu", 0);
+
+    std::optional<std::vector<CLIParser::InvokeableFunction>> parsedFunctions = parser.parse();
+    if (parsedFunctions) {
+        for (const auto& parsedFunction : *parsedFunctions) {
+            CLIParser::invokeFunction(parsedFunction.function, parsedFunction.arguments);
+        }
+    }
+    return 0;
+}
