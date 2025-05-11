c95554f92d409c567b04844b6873fc3e5e6dc548
Jason Fu <r.fu1@student.tue.nl>
2025-05-11 03:46:49 +0200

    remove old impl

--- core/include/arp_poisoning/arp_poisoning.h
+++ core/include/arp_poisoning/arp_poisoning.h
@@ -1,27 +1,4 @@
 #pragma once
 
-#include <string>
-
-namespace ATK::ARP {
-
-struct ArpPoisoningOptions {
-    std::string ipAttacker;
-    std::string macAttacker;
-    std::string ipVictim;
-    std::string macVictim;
-    std::string ipToSpoof;
-    std::string deviceName;
-};
-
-/**
- * Send a single arp poisoning packet.
- *
- * @param options strings containing MAC and ARP addresses
- * @throws invalid_argument if device is invalid
- * @throws invalid_argument if IP addresses are malformed
- * @throws invalid_argument if MAC addresses are malformed
- * @throws runtime_exception if construction of packet fails
- * @throws runtime_exception if sending of packet fails
- */
-void poisonArp(ArpPoisoningOptions &options);
+{
 } // namespace ATK::ARP
