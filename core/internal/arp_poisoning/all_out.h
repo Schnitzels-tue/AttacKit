f7dc3fc25922639faea74bef3795fe74d8492611
Jason Fu <r.fu1@student.tue.nl>
2025-05-11 05:37:17 +0200

    implementation all out

--- core/internal/arp_poisoning/all_out.h
+++ core/internal/arp_poisoning/all_out.h
@@ -1,17 +1,30 @@
 #include "MacAddress.h"
 #include "PcapLiveDevice.h"
 #include "arp_poisoning/arp_poisoning_strategy.h"
+#include <stdexcept>
+
 namespace ATK::ARP {
 class AllOutArpPoisoningStrategy : public ATK::ARP::ArpPoisoningStrategy {
   public:
     AllOutArpPoisoningStrategy(pcpp::PcapLiveDevice *device,
-                               pcpp::MacAddress attackerMacAddress)
-        : device_(device), attackerMacAddress_(attackerMacAddress) {}
+                               pcpp::MacAddress attackerMacAddress) {
+        if (device == nullptr) {
+            throw std::invalid_argument("Not a valid interface");
+        }
 
-    explicit AllOutArpPoisoningStrategy(pcpp::PcapLiveDevice *device)
-        : device_(device) {
-        attackerMacAddress_ = device_->getMacAddress();
+        device_ = device;
+        attackerMacAddress_ = attackerMacAddress;
     }
+
+    explicit AllOutArpPoisoningStrategy(pcpp::PcapLiveDevice *device) {
+        if (device == nullptr) {
+            throw std::invalid_argument("Not a valid interface");
+        }
+
+        device_ = device;
+        attackerMacAddress_ = device->getMacAddress();
+    }
+
     void execute() override;
 
   private:
