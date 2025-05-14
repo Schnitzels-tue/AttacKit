98e652a22e9e445b4aa3b9e894594cce95fc2af8
Jason Fu <95495174+fasonju@users.noreply.github.com>
2025-05-14 20:32:42 +0200

    Public silent interface (#26)
    * interface for silent poisoning strategy
    
    * hotfix

--- cli/src/cli.cpp
+++ cli/src/cli.cpp
@@ -2,7 +2,7 @@
 #include "arp_poisoning/public.h"
 
 int main(int argc, char *argv[]) {
-    ATK::ARP::AllOutPoisonOptions options{.ifaceIpOrName = "en0"};
+    ATK::ARP::AllOutPoisoningOptions options{.ifaceIpOrName = "en0"};
 
     ATK::ARP::allOutPoison(options);
 }
