10e88bbafd6dc52c9c3f9f3fe8807924d501a261
NickFris <n.frissen@student.tue.nl>
2025-05-16 22:31:05 +0200

    ¨fix¨

--- core/include/dns_poisoning/public.h
+++ core/include/dns_poisoning/public.h
@@ -5,8 +5,8 @@
 namespace ATK::DNS {
 struct AllOutPoisonOptions {
     std::string ifaceIpOrName; // network interface e.g. en0
-    std::string victimIp; // the IP of the DNS server to poison
-    std::string attackerIp; // the IP to reroute domains to
+    std::string victimIp;      // the IP of the DNS server to poison
+    std::string attackerIp;    // the IP to reroute domains to
     std::unordered_set<std::string> domainsToSpoof; // domains to reroute
 };
 
