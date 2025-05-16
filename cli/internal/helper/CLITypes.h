c3b4bfb885b303b551f5168dba257b27bad551d0
Quinnca77 <50358541+Quinnca77@users.noreply.github.com>
2025-05-17 00:38:36 +0200

    Document CLITypes

--- cli/internal/helper/CLITypes.h
+++ cli/internal/helper/CLITypes.h
@@ -4,8 +4,17 @@
 #include <string>
 #include <vector>
 
+/**
+ * Simple alias for a function that is meant to be passed to a flag.
+ * Although it says "any" function, it only accepts functions that return void
+ * and accept a pointer to a vector of strings as a parameter.
+ */
 using AnyFunction = std::function<void(const std::vector<std::string> &)>;
 
+/**
+ * Specific options set to a flag. Specifies a certain behaviour a flag should
+ * meet, which will be taken into consideration when the flags are executed.
+ */
 struct FlagOptions {
     bool sensitiveToQuiet = false;
     bool priorityFlag =
@@ -13,6 +22,10 @@ struct FlagOptions {
     // can be extended
 };
 
+/**
+ * General Flag type. It stores everything associated with a certain specific
+ * flag.
+ */
 struct Flag {
     std::string flagName;
     char flagChar;
@@ -22,6 +35,10 @@ struct Flag {
     FlagOptions options;
 };
 
+/**
+ * Similar to the Flag struct, but does not have flagChar since the parser has
+ * to calculate this by itself.
+ */
 struct UnparsedFlag {
     std::string flagName;
     AnyFunction flagFunction;
@@ -30,6 +47,16 @@ struct UnparsedFlag {
     FlagOptions options = {};
 };
 
+/**
+ * A function that can simply be called with its function and arguments.
+ *
+ * Example:
+ * InvocableFunction test;
+ * test.function(test.arguments)
+ *
+ * Be sure to keep track of the options since this might impact when or how the
+ * function within has to behave.
+ */
 struct InvocableFunction {
     AnyFunction function;
     std::vector<std::string> arguments;
