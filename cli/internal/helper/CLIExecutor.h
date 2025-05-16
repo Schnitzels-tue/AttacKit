0e338f13d2566b3c7ae582551a4f50aab6fc178e
Quinnca77 <50358541+Quinnca77@users.noreply.github.com>
2025-05-17 01:54:43 +0200

    Document CLIExecutor functions

--- cli/internal/helper/CLIExecutor.h
+++ cli/internal/helper/CLIExecutor.h
@@ -4,19 +4,46 @@
 #include <string>
 #include <vector>
 
+/**
+ * Meant to execute the functions returned by CLIParser. Functions added to
+ * flags are meant to be defined here for proper operational logic.
+ */
 class CLIExecutor {
+    // Whether to show the help menu (when set to true, will ignore all other
+    // instructions and simply show the help menu)
     bool help = false;
+
     bool quiet = false;
 
   public:
+    /**
+     * Function to invoke an ARP poisoning attack with the right parameters
+     */
     static void invokeArpPoison(std::vector<std::string> args);
+
     static void doMeaningfulThing(std::vector<std::string> args);
     /**
      * Will simply call the passed function with the given arguments.
      */
     static void invokeFunction(const AnyFunction &,
                                const std::vector<std::string> &);
+
+    /**
+     * Sets the help field boolean to the specified value.
+     */
     void setHelp(bool);
+
+    /**
+     * Sets the quiet field boolean to the specified value.
+     */
     void setQuiet(bool);
+
+    /**
+     * Handles the entire execution of the flags passed to the cli by using the
+     * passed parser.
+     *
+     * Will respect the order that flags are passed in, and respects the
+     * FlagOptions passed to each flag too.
+     */
     void execute(CLIParser &) const;
 };
