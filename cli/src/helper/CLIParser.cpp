82abc431950c1739b294860490f8daf10dee7066
Quinnca77 <50358541+Quinnca77@users.noreply.github.com>
2025-05-17 01:37:00 +0200

    Document CLIParser

--- cli/src/helper/CLIParser.cpp
+++ cli/src/helper/CLIParser.cpp
@@ -19,7 +19,12 @@ std::optional<std::vector<InvocableFunction>>
 CLIParser::flagsToFunctions(int &iteration, std::vector<Flag> &setFlags) {
     std::vector<InvocableFunction> parsedFunctions;
 
+    // Usually, this only goes for a single iteration, but in the case of
+    // stacked cli arguments (think of calling `git commit -am`) it will go into
+    // more iterations and parse from left to right
     for (const Flag &setFlag : setFlags) {
+        // In case the flag does not have any arguments, we immediately push the
+        // function
         if (setFlag.amountOfArguments.size() == 1 &&
             setFlag.amountOfArguments.find(0) !=
                 setFlag.amountOfArguments.end()) {
@@ -27,11 +32,16 @@ CLIParser::flagsToFunctions(int &iteration, std::vector<Flag> &setFlags) {
                 InvocableFunction{setFlag.flagFunction, {}, setFlag.options});
             continue;
         }
+
+        // Once here, the flag *might* need/have arguments
         std::vector<std::string> flagArgs;
         flagArgs.reserve(*setFlag.amountOfArguments.rbegin());
         const int beginIteration = iteration;
         for (int i = 0; i < *setFlag.amountOfArguments.rbegin(); ++i) {
             ++iteration;
+            // If we ran out of cli arguments but the provided amount of
+            // arguments does not match the flag's specification, we return an
+            // error.
             if (args.size() <= iteration &&
                 setFlag.amountOfArguments.find(iteration - beginIteration -
                                                1) ==
@@ -40,10 +50,14 @@ CLIParser::flagsToFunctions(int &iteration, std::vector<Flag> &setFlags) {
                           setFlag.flagName);
                 return std::nullopt;
             }
+            // We have found a good amount of arguments for this flag, and we
+            // have also run out of cli arguments
             if (args.size() <= iteration) {
                 --iteration;
                 break;
             }
+            // If we encounter a new flag while the encountered amount of
+            // arguments is not valid, we return an error
             if (args[iteration].rfind('-', 0) == 0 &&
                 setFlag.amountOfArguments.find(iteration - beginIteration -
                                                1) ==
@@ -52,6 +66,7 @@ CLIParser::flagsToFunctions(int &iteration, std::vector<Flag> &setFlags) {
                           setFlag.flagName);
                 return std::nullopt;
             }
+            // We found a new flag and the number of arguments is correct
             if (args[iteration].rfind('-', 0) == 0) {
                 --iteration;
                 break;
@@ -72,11 +87,16 @@ std::optional<std::vector<InvocableFunction>> CLIParser::parse() {
 
     for (int i = 0; i < args.size(); ++i) {
         std::vector<Flag> setFlags;
+        // Since inside this function, i should always be at a position where
+        // there is a flag, in cases where it doesn't we have run into a user
+        // error.
         if (args[i].at(0) != '-') {
             LOG_ERROR("Found argument without corresponding flag: " + args[i]);
             return std::nullopt;
         }
 
+        // If we have the name of the full flag (e.g. --help), extract the
+        // correct Flag struct
         if (args[i].length() > 2 && args[i].at(1) == '-') {
             const std::string flagName = args[i].substr(2, std::string::npos);
 
@@ -87,7 +107,8 @@ std::optional<std::vector<InvocableFunction>> CLIParser::parse() {
             }
 
             setFlags.push_back(allFlags[positionOfFlag]);
-        } else {
+        } else { // We are in the case where the user used a shortened flag
+                 // (e.g. -h)
             const std::string charFlags = args[i].substr(1, std::string::npos);
 
             for (const char charFlag : charFlags) {
@@ -102,11 +123,14 @@ std::optional<std::vector<InvocableFunction>> CLIParser::parse() {
             }
         }
 
+        // This case should be impossible, but it never hurts to check
         if (setFlags.empty()) {
             LOG_ERROR("Something went wrong while processing the command");
             return std::nullopt;
         }
 
+        // We parse the arguments passed to the flag(s) and check if that went
+        // right
         auto optionalParsedFunctions = flagsToFunctions(i, setFlags);
         if (!optionalParsedFunctions) {
             return std::nullopt;
@@ -151,6 +175,7 @@ void CLIParser::printHelp() {
 std::string CLIParser::generate_flags_text() {
     std::string flagsText;
 
+    // For each flag, construct its help text
     for (const auto &flag : allFlags) {
         flagsText.append("    -");
         flagsText += flag.flagChar;
@@ -165,6 +190,7 @@ std::string CLIParser::generate_flags_text() {
 }
 
 void CLIParser::add_flag(const UnparsedFlag &unparsedFlag) {
+    // Check if the person adding the flag is sane
     if (unparsedFlag.amountOfArguments.empty()) {
         LOG_ERROR("Amount of arguments was not specified correctly for flag " +
                   unparsedFlag.flagName);
@@ -173,6 +199,9 @@ void CLIParser::add_flag(const UnparsedFlag &unparsedFlag) {
     if (helpText.empty()) {
         LOG_WARN("No help text was provided for flag " + unparsedFlag.flagName);
     }
+
+    // Check whether the flag's name already exists or whether its first
+    // character is already taken
     std::unordered_set<char> takenChars;
     std::unordered_set<std::string> takenNames;
 
